4c92c79255dcc927def4c38b3e16792f8bc362a9
Josh Cohen <backpack360@gmail.com>
2023-04-19 09:35:35 -0700

    Clean up README and comments.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is a free browser extensions that plays YouTube Shorts in the normal YouTub
 
 It is designed for [Firefox](https://www.firefox.com/).
 
-After it's approved it will be downloadable [here](https://addons.mozilla.org/en-US/firefox/addon/youtube-shorts-normal-player/).
+Download page: [https://addons.mozilla.org/en-US/firefox/addon/youtube-shorts-normal-player/](https://addons.mozilla.org/en-US/firefox/addon/youtube-shorts-normal-player/).
--- background.js
+++ background.js
@@ -1,19 +1,16 @@
 browser.webRequest.onBeforeRequest.addListener(
     function (details) {
         const url = new URL(details.url);
+
         if (!url.pathname.startsWith("/shorts/")) return;
 
         const videoID = url.pathname.split("/shorts/")[1];
-
-        // I did it this way to ensure that if there are any existing params
-        // they all get printed out correctly with the `?` and `&` in the right
-        // places.
         url.searchParams.append("v", videoID);
         const newURL = new URL(url.origin);
         newURL.search = url.searchParams.toString();
         newURL.pathname = "/watch";
-
         const redirectUrl = newURL.toString();
+
         return {
             redirectUrl,
         };
