b78614589e378ff445765b3ab097c7cb1c3908e3
J <backpack360@gmail.com>
2023-10-20 13:36:53 -0700

    Get it working

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-redirect();
+redirect(window.location.pathname);
 
 document.addEventListener("yt-navigate-start", (e) => {
-    console.log("yt-navigate-start event", e);
     const path = e?.detail?.url;
     redirect(path);
 });
 
 function redirect(path) {
-    if (!path?.startsWith("/shorts/")) return;
+    const isShorts = path.startsWith("/shorts/");
+    if (!isShorts) return;
 
-    const newURL = new URL(path.origin);
-    newURL.searchParams = baseURL.searchParams;
+    const newURL = new URL(window.location);
+    newURL.searchParams = newURL.searchParams;
     const videoID = path.split("/shorts/")[1];
     newURL.searchParams.append("v", videoID);
     newURL.pathname = "/watch";
