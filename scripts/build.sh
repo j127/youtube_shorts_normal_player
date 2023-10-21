452e83bf937eb5338bade5206b2ba609db60b8da
J <backpack360@gmail.com>
2023-10-20 21:12:16 -0700

    Fix the manifest problem

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-# copy files into FF build dir
-# build FF
-# copy files into Chrome build dir
-# build Chrome
+echo "Building..."
+
+echo "Clearing _build dirs..."
+rm -rf _build
+mkdir -p _build/firefox/artifacts
+mkdir -p _build/chrome/artifacts
+
+echo "Copying code files..."
+cp -r src/* _build/firefox
+cp -r src/* _build/chrome
+
+echo "Copying manifest files..."
+cp manifests/firefox.json _build/firefox/manifest.json
+cp manifests/chrome.json _build/chrome/manifest.json
+
+echo "Copying licence..."
+cp LICENSE _build/firefox
+cp LICENSE _build/chrome
+
+echo "Building Firefox version..."
+pushd _build/firefox
+yarn build:firefox
+popd
+
+echo "Building Chrome version..."
+pushd _build/chrome
+yarn build:chrome
+popd
