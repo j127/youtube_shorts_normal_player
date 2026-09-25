f3657d9ba5f154c2387a6fc4c9a574e36e64e264
J <backpack360@gmail.com>
2026-09-24 22:48:59 -0700

    Update/create helper files
    These files are just for managing the repo.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,18 +1,11 @@
 /** @type {import("prettier").Config} */
 export default {
-    endOfLine: "lf",
-    semi: true,
-    singleQuote: false,
-    tabWidth: 4,
-    trailingComma: "es5",
-    arrowParens: "always",
-    plugins: [],
-    overrides: [
-        {
-            files: ["*.md", "*.mdx", "*.yaml", "*.yml"],
-            options: {
-                tabWidth: 2,
-            },
-        },
-    ],
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  arrowParens: "always",
+  plugins: [],
+  overrides: [],
 };
